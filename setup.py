ea8eec499c3373f77d637d689a030da09a33c771
Angel Chavez <angelgabrielchavez18@gmail.com>
2021-10-15 13:54:19 -0600

    update

--- setup.py
+++ setup.py
@@ -4,18 +4,17 @@ from setuptools import find_packages, setup
 with open("README.md", "r", encoding="utf-8") as fh:
     LONG_DESCRIPTION = fh.read()
 
-VERSION = '0.0.1' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
-PACKAGE_NAME = 'basstatpl' #Debe coincidir con el nombre de la carpeta 
-AUTHOR = 'Angel Gabriel Chavez Otzoy' #Modificar con vuestros datos
-AUTHOR_EMAIL = 'angelgabrielchavez18@gmail.com' #Modificar con vuestros datos
-URL = 'https://github.com/Angel-Gabriel-Chavez' #Modificar con vuestros datos
+VERSION = '0.0.1'
+PACKAGE_NAME = 'basstatpl'
+AUTHOR = 'Angel Gabriel Chavez Otzoy' 
+AUTHOR_EMAIL = 'angelgabrielchavez18@gmail.com' 
+URL = 'https://github.com/Angel-Gabriel-Chavez'
 
 LICENSE = 'MIT'
 DESCRIPTION = 'Library dedicated to performing different tabulations and basic statistical calculations.'
 LONG_DESC_TYPE = "text/markdown"
 
 
-#Paquetes necesarios para que funcione la libreía. Se instalarán a la vez si no lo tuvieras ya instalado
 INSTALL_REQUIRES = [
       'pymupdf',
       'pandas',
